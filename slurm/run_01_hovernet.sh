9a8a541b10c263c37ddbbdabca406da5b1d1c014
Sophia Li <sophiamjiali@gmail.com>
2026-07-06 17:45:14 -0400

    Added requirements

--- slurm/run_01_hovernet.sh
+++ slurm/run_01_hovernet.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+#SBATCH --output=/cluster/home/t144807uhn/logs/wsi-attention-composition/hovernet/%j.out
+#SBATCH --error=/cluster/home/t144807uhn/logs/wsi-attention-composition/hovernet/%j.err
+#SBATCH --account=kumargroup_gpu
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:30:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
 
 
-# request 6-17 CPUs and GPU
 
 
 # Add the cloned HoVer-net repository to the Python path
