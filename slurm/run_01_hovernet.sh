adb31cf3a097c5b3edff6a9f1253dfb777d687b9
Sophia Li <sophiamjiali@gmail.com>
2026-07-06 17:46:56 -0400

    Implemented SLURM submission script

--- slurm/run_01_hovernet.sh
+++ slurm/run_01_hovernet.sh
@@ -9,10 +9,29 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=32G
 
+# Make the project-specific logs directory
+mkdir -p /cluster/home/t144807uhn/logs/wsi-attention-composition/hovernet
 
+# Activate the virtual environment
+source /cluster/home/t144807uhn/envs/wsi-env/bin/activate
 
+# Ensure that all commands resolve back to the proper root directory
+cd /cluster/home/t144807uhn/wsi-attention-composition
 
 # Add the cloned HoVer-net repository to the Python path
 export PYTHONPATH="/cluster/projects/kumargroup/sophia/hover_net:$PYTHONPATH"
 
-...
+echo "=========================================="
+echo "Mini Sweep Job ID:  $SLURM_JOB_ID"
+echo "Job Name:           $1"
+echo "Node:               $SLURMD_NODENAME"
+echo "GPU:                $CUDA_VISIBLE_DEVICES"
+echo "Start time:         $(date)"
+echo "=========================================="
+
+srun python scripts/01_run_hovernet.py \
+    --config config.yaml
+
+echo "=========================================="
+echo "End time: $(date)"
+echo "=========================================="
